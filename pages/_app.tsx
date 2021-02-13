446138ce1e9f62c9e5e0b0016e463d8b84884c83
thunder-coding <yakshbari4@gmail.com>
2021-02-13 16:44:09 +0530

    Use prettier to run format code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,25 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-    <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <meta name="keywords" content="LUDO, LUDO Online, Free LUDO, Free LUDO Game, Free LUDO Game Online"/>
-      <meta name="description" content="A LUDO Game which you can play in your browser"/>
-    </Head>
-    <Component {...pageProps} />
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+                <meta
+                    name="keywords"
+                    content="LUDO, LUDO Online, Free LUDO, Free LUDO Game, Free LUDO Game Online"
+                />
+                <meta
+                    name="description"
+                    content="A LUDO Game which you can play in your browser"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ message: 'API is fine, hope so are you!' })
+    res.status(200).json({ message: 'API is fine, hope so are you!' })
 }
