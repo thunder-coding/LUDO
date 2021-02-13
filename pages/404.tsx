f76eb2c478f9ed9c637eefb40bc475131d741c12
thunder-coding <yakshbari4@gmail.com>
2021-02-13 16:56:24 +0530

    Prettier use tabs

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,7 @@
 export default function Page404() {
-    return (
-        <div>
-            <h1>404 Page not found</h1>
-        </div>
-    )
+	return (
+		<div>
+			<h1>404 Page not found</h1>
+		</div>
+	)
 }
