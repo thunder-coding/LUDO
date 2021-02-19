344ec9a573cc5d083ac9278065bbbbca482da1b3
thunder-coding <yakshbari4@gmail.com>
2021-02-19 19:32:01 +0530

    Remove meta tag from _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,6 @@ function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<Head>
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<meta
 					name="keywords"
 					content="LUDO, LUDO Online, Free LUDO, Free LUDO Game, Free LUDO Game Online"
