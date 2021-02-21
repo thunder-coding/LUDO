d4ae71be03e3851c76cca70239d1079dbaf2e383
thunder-coding <yakshbari4@gmail.com>
2021-02-21 16:28:09 +0530

    Added JSDoc comments to new.ts

--- pages/api/new.ts
+++ pages/api/new.ts
@@ -8,25 +8,64 @@ function generateToken() {
 	return token
 }
 
+/** This object represents a token */
 interface Token {
+	/** 
+	 * The unique token number of each token held by the player
+	 * <br>
+	 * Range 1 to 4
+	 */
 	_id: number
+	/**
+	 * The current position of the this token.
+	 *
+	 * Range:  -1 to 56
+	 *
+	 * -1 represents that the token has not yet started it's journey \
+	 *  0 represents that the token is on the first position relative to it's start
+	 */
 	position: number
 }
 
+/**This represents a player */
 interface Player {
+	/**
+	 * The unique id of the player
+	 *
+	 * Range: 1 to 4
+	 */
 	_id: number
+	/**
+	 * This authorisation token is required to perform validated requests like rolling the dice
+	 */
 	AuthorisationToken: string
+	/** This array represents all the token's owned by the player */
 	token: Token[]
 }
 
+/** This represents Player data as it should be visible to the client hiding the AuthorisationToken */
 interface VisiblePlayer {
+	/**
+	 * The unique id of the player
+	 * 
+	 * Range: 1 to 4
+	 */
 	_id: number
+	/** This array represents all the tokens owned by the player */
 	token: Token[]
 }
 
+/** This represents all the data that is to be stored by MongoDB */
 class Game {
+	/** The most number of player which join the current game */
 	player_limit: number
+	/** 
+	 * This represents the Authorisation Token of the owner of the game (The one who started it)
+	 * 
+	 * It has been stored seperately because the admin should have much more permissions than other players
+	 */
 	admin_token: string
+	/** This array represents all the players in the game */
 	player: Player[]
 	constructor(player_limit: number = 2, Res: NextApiResponse) {
 		if (player_limit > 4 || player_limit < 2) {
@@ -49,8 +88,11 @@ class Game {
 	}
 }
 
+/** This represents Game as it should be visible to the client, hiding all the AuthorisationTokens */
 class VisibleGame {
+	/** The most number of player which join the current game */
 	player_limit: number
+	/** This array represents all the players in the game */
 	player: VisiblePlayer[]
 	constructor(game: Game) {
 		this.player_limit = game.player_limit
