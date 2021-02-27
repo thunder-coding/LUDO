fa6d35f8ddb80a71b2b8cd06dcf65eca37ccd238
thunder-coding <yakshbari4@gmail.com>
2021-02-27 16:24:07 +0530

    Fix errors in Player.ts

--- lib/Player.ts
+++ lib/Player.ts
@@ -2,19 +2,17 @@ import Token from './Token'
 import { TokenColor } from './enums'
 
 /**This represents a player */
-export interface Player {
+export interface Player extends VisiblePlayer {
 	/**
 	 * This authorisation token is required to perform validated requests like rolling the dice
 	 */
 	AuthorisationToken: string
-	/** The color of the player */
-	color: keyof keyof TokenColor
-	/** This array represents all the token's owned by the player */
-	token: Token[]
 }
 
 /** This represents Player data as it should be visible to the client hiding the AuthorisationToken */
 export interface VisiblePlayer {
 	/** This array represents all the tokens owned by the player */
 	token: Token[]
+	/** The color of the player */
+	color: keyof keyof TokenColor
 }
